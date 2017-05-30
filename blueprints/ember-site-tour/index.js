a9ee537df3e0e6826f991bc0d1a9bd2086d5781a
Francesco Novy <francesco@cropster.com>
2017-05-30 10:40:10 +0200

    Add blueprint which adds hopscotch dependency

---
+++ blueprints/ember-site-tour/index.js
@@ -0,0 +1,17 @@
+/* eslint-env node */
+
+'use strict';
+
+module.exports = {
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
+
+  afterInstall: function() {
+    return this.addBowerPackagesToProject([
+      { name: 'hopscotch', target: '~0.2.8' }
+    ]);
+  }
+};

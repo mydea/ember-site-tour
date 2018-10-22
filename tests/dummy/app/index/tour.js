0b8a593dea567530ff8880e34dc0b3b32c6e147a
Francesco Novy <francesco@cropster.com>
2018-10-22 14:05:18 +0200

    Make dummy tour an EmberObject

--- tests/dummy/app/index/tour.js
+++ tests/dummy/app/index/tour.js
@@ -1,22 +1,28 @@
-export default [
-  {
-    target: '#installation',
-    title: 'Easy Installation',
-    content: 'Just enter this command.'
-  },
-  {
-    target: '#configuration',
-    title: 'Configuration',
-    content: 'There are many configuration options.'
-  },
-  {
-    target: '#defining-tours',
-    title: 'Defining Tours',
-    content: 'It is easy to define flexible & powerful tours.'
-  },
-  {
-    target: '#starting-tours',
-    title: 'Nice to Use',
-    content: 'Common usage is simple, while allowing fine grained control if necessary.'
-  }
-];
+import EmberObject, { computed } from '@ember/object';
+
+export default EmberObject.extend({
+  tour: computed(function() {
+    return [
+      {
+        target: '#installation',
+        title: 'Easy Installation',
+        content: 'Just enter this command.'
+      },
+      {
+        target: '#configuration',
+        title: 'Configuration',
+        content: 'There are many configuration options.'
+      },
+      {
+        target: '#defining-tours',
+        title: 'Defining Tours',
+        content: 'It is easy to define flexible & powerful tours.'
+      },
+      {
+        target: '#starting-tours',
+        title: 'Nice to Use',
+        content: 'Common usage is simple, while allowing fine grained control if necessary.'
+      }
+    ];
+  })
+});
